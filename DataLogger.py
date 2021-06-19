c0cb02e9239b434ec269f8157dca340678574f62
Yusuf <keklikyusuf@gmail.com>
2021-06-20 01:19:46 +0200

    CSV and TXT loggers has been added! Functionality of the logger has been finilized!

--- DataLogger.py
+++ DataLogger.py
@@ -11,27 +11,27 @@ import csv
 import time
 import random
 
-data = ['First Data', 'Second Data', 'Third Data']
+dataFrame = ['First Data', 'Second Data', 'Third Data']
+# Crete a dataframe that you want to log into csv file
+
+txtFrame = "That can be any string to log!"
+# Any type of variable can be pushed to log into txt file
 
 
-class DataLogger:
+class csvDataLogger:
     """
     It has been created to log CSV data type into TXT. Data can be adjusted according to user desire!
     """
-    def __init__(self, fileName, data):
+    def __init__(self, fileName, dataFrame):
         """
         :param fileName: Enter desired file name for your log file. -String
         Log file is being created with that time time-stamp and closed as soon as object is being generated!
         """
         self.fileName = fileName
-        now = datetime.datetime.now()
-        time = now.strftime("%d_%m_%Y-%H_%M_%S")
+        self.dataFrame = dataFrame.insert(0, "TimeStamp")
+        time = datetime.datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
         self.finalName = f'{self.fileName} {time}.txt'
-        self.newfile = open(f'{self.finalName}', "w+")
-        self.newfile.close()
-        self.data = data
-
-        self.data.insert(0, "TimeStamp")
+        open(f'{self.finalName}', "w+").close()
 
     def csvLogger(self):
         """
@@ -39,19 +39,38 @@ class DataLogger:
         :return: This function returns nothing
         It opens created log file and logs it according to desired logging.
         """
-        self.newfile = open(self.finalName, 'a', newline='')
-        write = csv.writer(self.newfile)
-        write.writerow(data)
-        self.newfile.close()
-        data[0] = time.strftime('%d-%m-%Y %H:%M:%S')
-        return data
+        csvFile = open(self.finalName, 'a', newline='')
+        write = csv.writer(csvFile)
+        write.writerow(dataFrame)
+        csvFile.close()
+        dataFrame[0] = time.strftime('%d-%m-%Y %H:%M:%S')
+        return dataFrame
+
+
+class txtDatalogger:
+
+    def __init__(self, fileName):
+        self.fileName = fileName
+        time = datetime.datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
+        self.finalName = f'{self.fileName} {time}.txt'
+        open(f'{self.finalName}', "w+").close()
+
+    def txtLogger(self, message):
+        txtMessage = f'{message} \n'
+        txtFile = open(self.finalName, 'a')
+        txtFile.write(txtMessage)
+        txtFile.close()
+        return message
 
 
 if __name__ == '__main__':
-    Data = DataLogger('Mylog', data)
+    csvLog = csvDataLogger('MyCSV', dataFrame)
+    txtLog = txtDatalogger('MyTXT')
+    print(csvLog.finalName)
+    print(txtLog.finalName)
     while True:
-        print(Data.finalName)
-        Data.csvLogger()
+        csvLog.csvLogger()
+        txtLog.txtLogger(txtFrame)
         time.sleep(0.5)
-        for i in range(1, len(data)):
-            data[i] = random.randint(0, 100)
+        for i in range(1, len(dataFrame)):
+            dataFrame[i] = random.randint(0, 100)
